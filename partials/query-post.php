3d759a652acd228945ccceb463923e671cef2681
mikeyrayvon <mikey@interglobal.vision>
2020-05-19 13:14:05 +0200

    link post query item

--- partials/query-post.php
+++ partials/query-post.php
@@ -4,16 +4,18 @@ $authors = get_post_meta($post->ID, '_igv_post_authors', true);
 ?>
 
 <article <?php post_class('grid-item item-s-6 item-m-4 item-l-3 text-align-center'); ?> id="post-<?php the_ID(); ?>">
-  <h3 class="font-size-mid"><em><?php echo !empty($title) ? $title : ''; ?></em></h3>
-  <div><span class="ornament">2</span></div>
-  <div><?php
-    if (!empty($authors)) {
-      foreach ($authors as $key => $value) {
-        echo '<span>';
-        echo $value;
-        echo ($key + 1) < count($authors) ? ', ' : '';
-        echo '</span>';
+  <a href="<?php the_permalink(); ?>">
+    <h3 class="font-size-mid"><em><?php echo !empty($title) ? $title : ''; ?></em></h3>
+    <div><span class="ornament">2</span></div>
+    <div><?php
+      if (!empty($authors)) {
+        foreach ($authors as $key => $value) {
+          echo '<span>';
+          echo $value;
+          echo ($key + 1) < count($authors) ? ', ' : '';
+          echo '</span>';
+        }
       }
-    }
-  ?></div>
+    ?></div>
+  </a>
 </article>
