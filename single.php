9cbf6d02f58d3287c7c598269c312be9404f3c69
mikeyrayvon <mikey@interglobal.vision>
2020-05-25 18:50:34 +0200

    add query to single

--- single.php
+++ single.php
@@ -4,9 +4,20 @@ get_header();
 if (have_posts()) {
   while (have_posts()) {
     the_post();
+    $top_id = $post->ID;
     get_template_part('partials/content-' . get_post_type());
   }
 }
+?>
+
+  </div>
+</section>
+
+<?php
+
+set_query_var( 'exclude_id', absint( $top_id ) );
+set_query_var( 'is_top', false );
+get_template_part('partials/query');
 
 get_footer();
 ?>
