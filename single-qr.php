932c017d38d59276003d39b158fc8fc4fd43f610
mikeyrayvon <mikey@interglobal.vision>
2022-05-30 12:31:28 +0200

    add fallback home url to qr

--- single-qr.php
+++ single-qr.php
@@ -1,6 +1,12 @@
 <?php
 $post_id = get_the_id();
 $url = get_post_meta($post_id, '_igv_qr_link', true);
-header("Location: " . $url);
+
+if (!empty($url)) {
+  header("Location: " . $url);
+} else {
+  header("Location: " . home_url());
+}
+
 die();
 ?>
